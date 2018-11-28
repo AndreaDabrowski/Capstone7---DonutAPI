9fd399e799be4dd6051fed648bb283291440ae4d
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-11-27 23:39:05 -0500

    MVP achieved

---
+++ DonutAPI/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DonutAPI.Models;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace DonutAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("https://grandcircusco.github.io/demo-apis/donuts.json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                //get response stream
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                //read repsonse as a string
+                string output = reader.ReadToEnd();//output to put into JSON in a sec
+                //reader.Close(); do it below or up here
+
+                //convert response to JSON
+                JObject parser = JObject.Parse(output);//gives an array of objects
+
+                reader.Close();
+                response.Close();
+                
+                TempData.Add("donuts", parser["results"]);
+                return View();
+            }
+            ViewBag.Error = "Something Went Wrong!";
+            return View();
+        }
+        public ActionResult DonutDeets(string id)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp("https://grandcircusco.github.io/demo-apis/donuts/"+id+".json");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string output = reader.ReadToEnd();//output to put into JSON in a sec
+                JObject parser = JObject.Parse(output);//gives an array of objects
+
+                reader.Close();
+                response.Close();
+
+                ViewBag.ThisDonut = parser;
+                return View();
+            }
+            ViewBag.Error = "Something Went Wrong!";
+            return View();
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DonutAPI/Models/Donut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DonutAPI.Models
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string Extras { get; set; }
+        public string Image { get; set; }
+    }
+}
